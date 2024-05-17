995545814699519634a17ec6e8bf71a0400ca3aa
foley61 <coder61616@gmail.com>
2024-05-17 14:00:27 +0300

    up

--- src/components/profile-button/index.jsx
+++ src/components/profile-button/index.jsx
@@ -4,12 +4,9 @@ import * as S from "./styles";
 function ProfileButton() {
   return (
     <S.ProfileButton>
-      <S.Image
-        src="https://avatars.githubusercontent.com/u/60665510?v=4"
-        alt="Mehmet Pekcan"
-      />
+   
       <S.Content>
-        <S.Text>Mehmet Pekcan</S.Text>
+        <S.Text>User</S.Text>
         <S.Subtitle>Admin</S.Subtitle>
       </S.Content>
     </S.ProfileButton>
